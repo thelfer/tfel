ba7239f28461b8e05a1bb975c54b9bf31d66dae5
th202608 <th202608@localhost>
2010-07-12 06:29:46 +0000

    correct autoconf/automake (gettext s****...)

--- clean.sh
+++ clean.sh
@@ -17,6 +17,10 @@ rm -f  m4/lt~obsolete.m4
 rm -f  m4/ltversion.m4
 rm -f  m4/ltsugar.m4
 rm -f  m4/libtool.m4
+rm -f m4/lib-link.m4
+rm -f m4/gettext.m4
+rm -f m4/iconv.m4
+rm -f m4/po.m4
 rm -f  configure aclocal.m4 
 rm -f  $(find . -name "*~")
 rm -f  $(find . -name "Makefile.in")
