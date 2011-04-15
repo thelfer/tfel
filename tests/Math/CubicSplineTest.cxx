2403f8c02c32c4ee09df06c4256822e25f59490f
th202608 <th202608@localhost>
2011-04-15 12:07:50 +0000

    add CubicSplineTest

---
+++ tests/Math/CubicSplineTest.cxx
@@ -0,0 +1,129 @@
+/*! 
+ * \file   CubicSplineTest.cxx
+ * \brief
+ * \author Helfer Thomas
+ * \brief  12 avr 2011
+ */
+
+#include<cmath>
+#include<fstream>
+#include<cmath>
+
+#include"TFEL/Tests/TestCase.hxx"
+#include"TFEL/Tests/TestProxy.hxx"
+#include"TFEL/Tests/TestManager.hxx"
+
+#include"TFEL/Math/tvector.hxx"
+#include"TFEL/Math/CubicSpline.hxx"
+
+struct CubicSplineTest
+  : public tfel::tests::TestCase
+{
+  CubicSplineTest()
+    : tfel::tests::TestCase("TFEL/Math",
+			    "CubicSplineTest")
+  {} // end of CubicSplineTest
+
+  template<double (*F)(double),
+	   double (*f)(double),
+	   double (*df)(double)>
+  static double computeIntegral(const double xa,
+				const double xb)
+  {
+    using namespace std;
+    if(xb<xa){
+      return -computeIntegral<F,f,df>(xb,xa);
+    }
+    if(xa<-1){
+      double d=df(-1);
+      double y=f(-1);
+      double s=y*(-1.-xa)-0.5*d*(xa+1)*(xa+1);
+      return s+computeIntegral<F,f,df>(-1.,xb);
+    }
+    if(xb>1){
+      double d=df(1);
+      double y=f(1);
+      double s=y*(xb-1.)+0.5*d*(xb-1.)*(xb-1.);
+      return s+computeIntegral<F,f,df>(xa,1.);
+    }
+    return F(xb)-F(xa);
+  }
+
+  static double msin(double x){
+    using namespace std;
+    return -sin(x);
+  }
+
+  static double mcos(double x){
+    using namespace std;
+    return -cos(x);
+  }
+
+  tfel::tests::TestResult
+  execute()
+  {
+    using namespace std;
+    using namespace tfel::math;
+    const vector<double>::size_type n = 10;
+    vector<double> x(n);
+    vector<tvector<2u,double> > y(n);
+    vector<double>::size_type i;
+    for(i=0;i!=x.size();++i){
+      x[i] = -1.+(2./static_cast<double>(x.size()-1))*static_cast<double>(i);
+      y[i][0] = cos(x[i]);
+      y[i][1] = sin(x[i]);
+    }
+    CubicSpline<double,tvector<2u,double> > spline;
+    spline.setCollocationPoints(x,y);
+    ofstream out;
+    out.open("CubicSplineTest.out");
+    if(!out){
+      string msg("CubicSplineTest::execute : ");
+      msg += "can't open file 'CubicSplineTest.out'";
+      throw(runtime_error(msg));
+    }
+    out << endl;
+    for(i=0;i!=2*x.size();++i){
+      double x1 = -1.+(2./static_cast<double>(2*x.size()-1))*static_cast<double>(i);
+      tvector<2u> y1;
+      y1 = spline.getValue(x1);
+      out << x1 << " " << y1[0] << " " << y1[1] << endl;
+    }
+    const double eps = 1.e-2;
+    tvector<2u,double> in1 = spline.computeIntegral(-1,1);
+    TFEL_TESTS_ASSERT(abs(in1[0]-computeIntegral<sin,cos,msin>(-1,1))<eps);
+    TFEL_TESTS_ASSERT(abs(in1[1]-computeIntegral<mcos,sin,cos>(-1,1))<eps);
+    tvector<2u,double> in2 = spline.computeIntegral(-1.25,-1);
+    TFEL_TESTS_ASSERT(abs(in2[0]-computeIntegral<sin,cos,msin>(-1.25,-1))<eps);
+    TFEL_TESTS_ASSERT(abs(in2[1]-computeIntegral<mcos,sin,cos>(-1.25,-1))<eps);
+    tvector<2u,double> in3 = spline.computeIntegral(1.,1.25);
+    TFEL_TESTS_ASSERT(abs(in3[0]-computeIntegral<sin,cos,msin>(1.,1.25))<eps);
+    TFEL_TESTS_ASSERT(abs(in3[1]-computeIntegral<mcos,sin,cos>(1.,1.25))<eps);
+    tvector<2u,double> in4 = spline.computeIntegral(-1.25,1.25);
+    TFEL_TESTS_ASSERT(abs(in4[0]-computeIntegral<sin,cos,msin>(-1.25,1.25))<eps);
+    TFEL_TESTS_ASSERT(abs(in4[1]-computeIntegral<mcos,sin,cos>(-1.25,1.25))<eps);
+    tvector<2u,double> in5 = spline.computeIntegral(-0.015,0.01);
+    TFEL_TESTS_ASSERT(abs(in5[0]-computeIntegral<sin,cos,msin>(-0.015,0.01))<eps);
+    TFEL_TESTS_ASSERT(abs(in5[1]-computeIntegral<mcos,sin,cos>(-0.015,0.01))<eps);
+    return this->result;
+  } // end of execute
+};
+
+TFEL_TESTS_GENERATE_PROXY(CubicSplineTest,
+			  "CubicSplineTest");
+
+int main(void)
+{
+  using namespace std;
+  using namespace std;
+  using namespace tfel::tests;
+  using namespace tfel::utilities;
+  TestManager& manager = TestManager::getTestManager();
+  manager.addTestOutput(cout);
+  manager.addXMLTestOutput("CubicSpline.xml");
+  TestResult r = manager.execute();
+  if(!r.success()){
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+} // end of main
