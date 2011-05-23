5929faebd94ef0fe5aaf9487aa55d1319285755f
th202608 <th202608@localhost>
2011-05-23 09:34:45 +0000

    add the TFEL/Macros.hxx file

--- include/TFEL/Metaprogramming/StaticAssert.hxx
+++ include/TFEL/Metaprogramming/StaticAssert.hxx
@@ -8,6 +8,7 @@
 #ifndef _LIB_TFEL_STATICASSERT_H_
 #define _LIB_TFEL_STATICASSERT_H_ 
 
+#include"TFEL/Macros.hxx"
 #include"TFEL/Config/TFELConfig.hxx"
 
 namespace tfel{
@@ -46,42 +47,6 @@ namespace tfel{
   } // end of namespace meta
 
 } // end of namespace tfel
-
-/* !
- * \def TFEL_PP_JOIN
- * \brief this macro joins joins its two arguments together.
- *
- * This macro was taken from the boost library:
- * - http://boost.org/
- * 
- * The following piece of macro magic joins the two
- * arguments together, even when one of the arguments is
- * itself a macro (see 16.3.1 in C++ standard).  The key
- * is that macro expansion of macro arguments does not
- * occur in TFEL_PP_DO_JOIN2 but does in TFEL_PP_DO_JOIN.
- *
- * \author Helfer Thomas
- * \date   28 Aug 2006
- */
-#define TFEL_PP_JOIN( X, Y ) TFEL_PP_DO_JOIN( X, Y )
-
-/*!
- * \def TFEL_PP_DO_JOIN
- * \brief An helper macro for TFEL_PP_JOIN
- * \see TFEL_PP_JOIN
- * \author Helfer Thomas
- * \date   28 Aug 2006
- */
-#define TFEL_PP_DO_JOIN( X, Y ) TFEL_PP_DO_JOIN2(X,Y)
-
-/*!
- * \def TFEL_PP_DO_JOIN2
- * \brief An helper macro for TFEL_PP_JOIN
- * \see TFEL_PP_JOIN
- * \author Helfer Thomas
- * \date   28 Aug 2006
- */
-#define TFEL_PP_DO_JOIN2( X, Y ) X##Y
 
 #ifndef TFEL_NOSTATICDEBUG 
 /*!
