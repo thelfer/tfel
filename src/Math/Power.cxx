790639f10639c4cb7aba948fabb5fac67ad3c154
th202608 <th202608@localhost>
2012-01-16 09:10:58 +0000

    add missing file

---
+++ src/Math/Power.cxx
@@ -0,0 +1,26 @@
+/*!
+ * \file   Power.cxx
+ * \brief  
+ * 
+ * \author Helfer Thomas
+ * \date   06 jan 2007
+ */
+
+#include<cmath>
+
+#include"TFEL/Config/TFELConfig.hxx"
+#include"TFEL/Math/Function/Power.hxx"
+
+namespace tfel{
+
+  namespace math{
+
+    namespace stdfunctions{
+
+      TFEL_VISIBILITY_EXPORT tfel::math::FctSqrt sqrt;
+
+    } // end of namespace stdfunctions
+
+  }  // end of namespace math
+
+}  // end of namespace tfel
