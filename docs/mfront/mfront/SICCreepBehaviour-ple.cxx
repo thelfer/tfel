d94e074e29dfbb1b9d92550f5ce80509444b636f
th202608 <th202608@localhost>
2012-11-23 14:54:07 +0000

    add doc

---
+++ docs/mfront/mfront/SICCreepBehaviour-ple.cxx
@@ -0,0 +1,17 @@
+/*
+ * \file   pleiades/SiCCreep/SiCCreep.ple
+ * \brief  example of how to use the SiCCreep behaviour law
+ * in the celaeno fuel performanece code
+ * \author Helfer Thomas
+ * \date   06 / 12 / 07
+ */
+
+MechanicalBehaviour 'IsotropicUmatBehaviour' 'libUmatBehaviour.so' 'umatSiCCreep' 
+MaterialProperty<constant>     'YoungModulus'    150.e9
+MaterialProperty<constant>     'PoissonRatio'      0.3
+MaterialProperty<constant>     'ThermalExpansion'  0.
+MaterialProperty<constant>     'MassDensity'       0.
+InternalStateVariable<Stensor> 'ElasticStrain'
+InternalStateVariable<Scalar>  'EquivalentViscoplasticStrain'
+EndOfMechanicalBehaviour
+EndOfMaterial
