da5e193933ea45fe05900ced21e890c38ebe02c8
th202608 <th202608@localhost>
2013-06-03 08:55:08 +0000

    add tests on polycristals, update de PolyCristalsSlidingSystems class to be able to read phases orientations in a file

--- include/TFEL/Material/PolyCristalsSlidingSystems.hxx
+++ include/TFEL/Material/PolyCristalsSlidingSystems.hxx
@@ -7,7 +7,9 @@
 
 #ifndef TFEL_MATERIAL_POLYCRISTALSSLIDINGSYSTEMS_HXX
 #define TFEL_MATERIAL_POLYCRISTALSSLIDINGSYSTEMS_HXX 1
-  
+
+#include<string> 
+
 #include"TFEL/Math/vector.hxx"
 #include"TFEL/Math/stensor.hxx"
 #include"TFEL/Math/General/RandomRotationMatrix.hxx"
@@ -36,15 +38,16 @@ namespace tfel
       typedef tfel::math::stensor<3u> StrainStensor;
       //! return the uniq instance of the class
       static const PolyCristalsSlidingSystems&
-      getPolyCristalsSlidingSystems();
+      getPolyCristalsSlidingSystems(const std::string& = "");
       //! tensor of directional senses, sorted by phases
       tfel::math::vector<tfel::math::vector<StrainStensor> > mus;
     private:
       /*!
        * default constructor (declared private~: it can only be used
        * by the getPolyCristalsSlidingSystems method)
+       * \param[in] f : file where the angles are declared
        */
-      PolyCristalsSlidingSystems();
+      PolyCristalsSlidingSystems(const std::string&);
       /*!
        * copy constructor (declared private to disable it)
        */
