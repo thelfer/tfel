57db3a80946b6f94d5cef71ac2e3d504b68ca135
th202608 <th202608@localhost>
2013-07-03 07:21:55 +0000

    add the ftree-vectorize and ffast-math optimisation flag (experimental). This lead to great performance improvements

--- cmake/modules/gcc.cmake
+++ cmake/modules/gcc.cmake
@@ -6,6 +6,8 @@ if(NOT i586-mingw32msvc_COMPILER)
   tfel_enable_cxx_compiler_flag(OPTIMISATION_FLAGS "fvisibility-inlines-hidden")
 endif(NOT i586-mingw32msvc_COMPILER)
 tfel_enable_cxx_compiler_flag(OPTIMISATION_FLAGS "march=native")
+tfel_enable_cxx_compiler_flag(OPTIMISATION_FLAGS "ftree-vectorize")
+tfel_enable_cxx_compiler_flag(OPTIMISATION_FLAGS "ffast-math")
 
 set(OPTIMISATION_FLAGS "-DNO_RUNTIME_CHECK_BOUNDS ${OPTIMISATION_FLAGS}")
 
