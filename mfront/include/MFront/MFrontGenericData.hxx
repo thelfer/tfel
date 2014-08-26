f381172bd9ec063461ad1e1ab9f0bcc626cea92a
af063744 <af063744@localhost>
2014-08-26 10:56:03 +0000

    Commit original : th202608  2014-03-10 10:11:41 +0100 (lun. 10 mars 2014)
    fixes recursive inclusion

--- mfront/include/MFront/MFrontGenericData.hxx
+++ mfront/include/MFront/MFrontGenericData.hxx
@@ -8,7 +8,7 @@
 #ifndef _LIB_MFRONT_MFRONTGENERICDATA_H_
 #define _LIB_MFRONT_MFRONTGENERICDATA_H_ 
 
-#warning "This header is deprected, one shall use 'MFront/MFrontGenericData.hxx' instead"
-#include"MFront/MFrontGenericData.hxx"
+#warning "This header is deprected, one shall use 'MFront/MFrontFileDescription.hxx' instead"
+#include"MFront/MFrontFileDescription.hxx"
 
 #endif /* _LIB_MFRONT_MFRONTGENERICDATA_H */
