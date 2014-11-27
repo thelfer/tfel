61b23c7c6c6320090a0e089d942c9f69e66cd41e
th202608 <th202608@localhost>
2014-11-27 11:14:27 +0000

    pandoc detection

--- cmake/modules/pandoc.cmake
+++ cmake/modules/pandoc.cmake
@@ -1,10 +1,12 @@
 find_program(PANDOC NAMES pandoc pandoc.exe)
+find_program(PANDOC_CITEPROC NAMES pandoc-citeproc pandoc-citeprocx.exe)
 mark_as_advanced(PANDOC)
-if(PANDOC)
+mark_as_advanced(PANDOC_CITEPROC)
+if(PANDOC AND PANDOC_CITEPROC)
     message(STATUS "Found Pandoc")
     # execute_process(
     #     COMMAND ${PANDOC} --version
     #     OUTPUT_VARIABLE PANDOC_VERSION_TEXT
     #     ERROR_VARIABLE PANDOC_VERSION_TEXT)
     set(HAVE_PANDOC ON)
-endif(PANDOC)
+endif(PANDOC AND PANDOC_CITEPROC)
