f53d446c4fcb54007c8fbe7b3ef941dd38f91d3c
th202608 <th202608@localhost>
2015-05-19 15:56:30 +0000

    small fixes

--- mfront/src/MFrontUMATInterface.cxx
+++ mfront/src/MFrontUMATInterface.cxx
@@ -2098,12 +2098,14 @@ namespace mfront{
       mcoel << ";";
       writeGibianeInstruction(out,mcoel.str());
       out << endl;
-      ostringstream mstatev;
-      mstatev << "statev = 'MOTS' ";
-      this->writeVariableDescriptionContainerToGibiane(mstatev,*ph,persistentVarsHolder);
-      mstatev << ";";
-      writeGibianeInstruction(out,mstatev.str());
-      out << endl;
+      if(!persistentVarsHolder.empty()){
+	ostringstream mstatev;
+	mstatev << "statev = 'MOTS' ";
+	this->writeVariableDescriptionContainerToGibiane(mstatev,*ph,persistentVarsHolder);
+	mstatev << ";";
+	writeGibianeInstruction(out,mstatev.str());
+	out << endl;
+      }
       ostringstream mparam;
       mparam << "params = 'MOTS' 'T'";
       if(!externalStateVarsHolder.empty()){
@@ -2117,7 +2119,9 @@ namespace mfront{
       mmod << "MO = 'MODELISER' v 'MECANIQUE' 'ELASTIQUE' ";
       mmod << nonlin << " 'NUME_LOI' 1 ";
       mmod << "'C_MATERIAU' coel ";
-      mmod << "'C_VARINTER' statev ";
+      if(!persistentVarsHolder.empty()){
+	mmod << "'C_VARINTER' statev ";
+      }
       mmod << "'PARA_LOI'   params 'CONS' M;";
       writeGibianeInstruction(out,mmod.str());
       out << endl;
