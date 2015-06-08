f407c40e6b50d9ddf6ed102c9ea0fd924d609746
th202608 <th202608@localhost>
2015-06-08 13:19:13 +0000

    fixes tests without requiring new automake keywords (serial-tests is not suppported by previous versions of automake)

--- tests/System/rwstream.cxx
+++ tests/System/rwstream.cxx
@@ -1,9 +1,9 @@
 /*!
- * \file   tests/System/rstream.cxx
+ * \file   tests/System/rwstream.cxx
  * \brief  
  * 
  * \author Helfer Thomas
- * \date   13 nov 2007
+ * \date   18 nov 2007
  * \copyright Copyright (C) 2006-2014 CEA/DEN, EDF R&D. All rights 
  * reserved. 
  * This project is publicly released under either the GNU GPL Licence 
@@ -12,40 +12,44 @@
  * project under specific licensing conditions. 
  */
 
-
 #ifdef NDEBUG
 #undef NDEBUG
 #endif /* NDEBUG */
 
-#include<cmath>
-#include<cassert>
 #include<cstdlib>
+#include<cassert>
 #include<cstring>
 
+#include"TFEL/System/wfstream.hxx"
 #include"TFEL/System/rfstream.hxx"
+
+static void write(void){
+  tfel::system::wfstream f("test.bin");
+  f << "toto" << 12.;
+  f << "tutu";
+  f.close();
+}
+
+static void read (void)
+{
+  tfel::system::rfstream r("test.bin");
+  char c[5];
+  double t;
+  r.read(c,4u);
+  c[4]='\0';
+  assert(strcmp(c,"toto")==0);
+  r >> t;
+  assert(abs(t-12.)<1.e-14);
+  r.read(c,4u);
+  c[4]='\0';
+  assert(strcmp(c,"tutu")==0);
+  r.close();
+} // end of main
 
 int
 main(void)
 {
-  using namespace std;
-  using namespace tfel::system;
-  
-  rfstream r("test.bin");
-  char c[5];
-  double t;
-  
-  r.read(c,4u);
-  c[4]='\0';
-  assert(strcmp(c,"toto")==0);
-
-  r >> t;
-  assert(abs(t-12.)<1.e-14);
-
-  r.read(c,4u);
-  c[4]='\0';
-  assert(strcmp(c,"tutu")==0);
-
-  r.close();
-  
+  write();
+  read();
   return EXIT_SUCCESS;
-} // end of main
+}
