4c961391fac7622c7f3a02aae93c776991e00e9d
th202608 <th202608@localhost>
2016-06-16 09:08:13 +0000

    undo unwanted commit

--- bindings/python/mtest/CurrentState.cxx
+++ bindings/python/mtest/CurrentState.cxx
@@ -15,6 +15,27 @@
 #include"MTest/CurrentState.hxx"
 #include"TFEL/Python/VectorConverter.hxx"
 
+#define TFEL_PYTHON_CURRENTSTATEGETTER( X )		\
+  static tfel::math::vector<mtest::real>		\
+  CurrentState_get##X(const mtest::CurrentState& t)	\
+  {                                                     \
+    return t. X ;                                       \
+  }
+
+TFEL_PYTHON_CURRENTSTATEGETTER(s_1)
+TFEL_PYTHON_CURRENTSTATEGETTER(s0)
+TFEL_PYTHON_CURRENTSTATEGETTER(s1)
+TFEL_PYTHON_CURRENTSTATEGETTER(e0)
+TFEL_PYTHON_CURRENTSTATEGETTER(e1)
+TFEL_PYTHON_CURRENTSTATEGETTER(e_th0)
+TFEL_PYTHON_CURRENTSTATEGETTER(e_th1)
+TFEL_PYTHON_CURRENTSTATEGETTER(mprops1)
+TFEL_PYTHON_CURRENTSTATEGETTER(iv_1)
+TFEL_PYTHON_CURRENTSTATEGETTER(iv0)
+TFEL_PYTHON_CURRENTSTATEGETTER(iv1)
+TFEL_PYTHON_CURRENTSTATEGETTER(esv0)
+TFEL_PYTHON_CURRENTSTATEGETTER(desv)
+
 static tfel::math::vector<mtest::CurrentState>::iterator
 v_begin(tfel::math::vector<mtest::CurrentState>& s){
   return s.begin();
@@ -30,7 +51,8 @@ v_setitem(tfel::math::vector<mtest::CurrentState>& s,
 	  const tfel::math::vector<mtest::CurrentState>::size_type i,
 	  const mtest::CurrentState& v){
   if(i>=s.size()){
-    throw(std::out_of_range("tfel::math::vector<mtest::CurrentState>::operator[]: invalid_index"));
+    throw(std::out_of_range("tfel::math::vector<mtest::CurrentState>::operator[]: "
+			    "invalid_index"));
   }
   s[i] = v;
 }
@@ -39,7 +61,8 @@ static mtest::CurrentState&
 v_getitem(tfel::math::vector<mtest::CurrentState>& s,
 	  const tfel::math::vector<mtest::CurrentState>::size_type i){
   if(i>=s.size()){
-    throw(std::out_of_range("tfel::math::vector<mtest::CurrentState>::operator[]: invalid_index"));
+    throw(std::out_of_range("tfel::math::vector<mtest::CurrentState>::operator[]: "
+			    "invalid_index"));
   }
   return s[i];
 }
@@ -60,26 +83,25 @@ void declareCurrentState(void)
   using namespace tfel::python;
 
   class_<mtest::CurrentState>("CurrentState")
-    .add_property("s_1",&mtest::CurrentState::s_1)
-    .add_property("s0",&mtest::CurrentState::s0)
-    .add_property("s1",&mtest::CurrentState::s1)
-    .add_property("e0",&mtest::CurrentState::e0)
-    .add_property("e1",&mtest::CurrentState::e1)
-    .add_property("e_th0",&mtest::CurrentState::e_th0)
-    .add_property("e_th1",&mtest::CurrentState::e_th1)
-    .add_property("mprops1",&mtest::CurrentState::mprops1)
-    .add_property("iv_1",&mtest::CurrentState::iv_1)
-    .add_property("iv0",&mtest::CurrentState::iv0)
-    .add_property("iv1",&mtest::CurrentState::iv1)
-    .add_property("esv0",&mtest::CurrentState::esv0)
-    .add_property("desv",&mtest::CurrentState::desv)
+    .add_property("s_1",CurrentState_gets_1)
+    .add_property("s0",CurrentState_gets0)
+    .add_property("s1",CurrentState_gets1)
+    .add_property("e0",CurrentState_gete0)
+    .add_property("e1",CurrentState_gete1)
+    .add_property("e_th0",CurrentState_gete_th0)
+    .add_property("e_th1",CurrentState_gete_th1)
+    .add_property("mprops1",CurrentState_getmprops1)
+    .add_property("iv_1",CurrentState_getiv_1)
+    .add_property("iv0",CurrentState_getiv0)
+    .add_property("iv1",CurrentState_getiv1)
+    .add_property("evs0",CurrentState_getesv0)
+    .add_property("desv",CurrentState_getdesv)
     ;
 
   class_<tfel::math::vector<mtest::CurrentState>>("CurrentStateVector")
     .def("__iter__",boost::python::range(&v_begin,&v_end))
     .def("__len__",&v_size)
-    .def("__getitem__",&v_getitem,
-	 return_internal_reference<>())
+    .def("__getitem__",&v_getitem,return_internal_reference<>())
     .def("__setitem__",&v_setitem)
     ;
   
