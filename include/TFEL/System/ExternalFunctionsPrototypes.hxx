6b1516b1ed03d280ad47d92fa7421353fdbc0bd1
th202608 <th202608@localhost>
2016-11-24 08:51:11 +0000

    Update the gallery: make its installation part of the build procedure

--- include/TFEL/System/ExternalFunctionsPrototypes.hxx
+++ include/TFEL/System/ExternalFunctionsPrototypes.hxx
@@ -423,25 +423,25 @@ namespace tfel
        * This is the prototype of the external functions used by the
        * pleiades version of the castem finite element solver
        */
-      typedef void (TFEL_ADDCALL_PTR EuroplexusFctPtr)(EuroplexusIntegerType *const,     // status of the integration, 0 on success
-						       EuroplexusRealType *const,        // Cauchy stress
-						       EuroplexusRealType *const,        // state variables
-						       EuroplexusRealType *const,        // stiffness tensor	  
-						       EuroplexusRealType *const,        // time step scaling factor
-						       EuroplexusIntegerType *const,     // broken indicator
-						       char *const,        // error message
+      typedef void (TFEL_ADDCALL_PTR EuroplexusFctPtr)(EuroplexusIntegerType *const,         // status of the integration, 0 on success
+						       EuroplexusRealType *const,            // Cauchy stress
+						       EuroplexusRealType *const,            // state variables
+						       EuroplexusRealType *const,            // stiffness tensor	  
+						       EuroplexusRealType *const,            // time step scaling factor
+						       EuroplexusIntegerType *const,         // broken indicator
+						       char *const,                          // error message
 						       const EuroplexusIntegerType  *const,  // number of state variables
 						       const EuroplexusIntegerType  *const,  // modelling hypothesis
-						       const EuroplexusRealType *const,  // time increment
-						       const EuroplexusRealType *const,  // deformation gradient at the beginning of the time step
-						       const EuroplexusRealType *const,  // deformation gradient at the end of the time step
-						       const EuroplexusRealType *const,  // rotation matrix
-						       const EuroplexusRealType *const,  // material properties
+						       const EuroplexusRealType *const,      // time increment
+						       const EuroplexusRealType *const,      // deformation gradient at the beginning of the time step
+						       const EuroplexusRealType *const,      // deformation gradient at the end of the time step
+						       const EuroplexusRealType *const,      // rotation matrix
+						       const EuroplexusRealType *const,      // material properties
 						       const EuroplexusIntegerType  *const,  // number of material properties
-						       const EuroplexusRealType *const,  // temperature
-						       const EuroplexusRealType *const,  // increment of temperature
-						       const EuroplexusRealType *const,  // external state variables
-						       const EuroplexusRealType *const,  // external state variables increments
+						       const EuroplexusRealType *const,      // temperature
+						       const EuroplexusRealType *const,      // increment of temperature
+						       const EuroplexusRealType *const,      // external state variables
+						       const EuroplexusRealType *const,      // external state variables increments
 						       const EuroplexusIntegerType  *const); // number of external state variables
       
     }
