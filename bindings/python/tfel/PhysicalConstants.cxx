0a0131efb030de3a2879817e2f8c9d5f2b843528
th202608 <th202608@localhost>
2017-08-18 14:44:52 +0000

    Fix Ticket #17: Add physical constants from CODATA

---
+++ bindings/python/tfel/PhysicalConstants.cxx
@@ -0,0 +1,91 @@
+/*!
+ * \file   bindings/python/tfel/PhysicalConstants.cxx
+ * \brief
+ * \author Thomas Helfer
+ * \date   18 août 2017
+ * \copyright Copyright (C) 2006-2014 CEA/DEN, EDF R&D. All rights 
+ * reserved. 
+ * This project is publicly released under either the GNU GPL Licence 
+ * or the CECILL-A licence. A copy of thoses licences are delivered 
+ * with the sources of TFEL. CEA or EDF may also distribute this 
+ * project under specific licensing conditions. 
+ */
+
+#include<boost/python.hpp>
+#include"TFEL/Config/TFELConfig.hxx"
+
+void declarePhysicalConstants();
+
+namespace tfel{
+  struct TFEL_VISIBILITY_EXPORT PhysicalConstants{
+//! Atomic mass constant (kg)
+static const double mu;
+//! Avogadro constant (mol-1)
+static const double Na;
+//! Boltzmann constant (J.K-1)
+static const double kb;
+//! Conductance quantum (S)
+static const double G0;
+//! Electric constant (F.m-1)
+static const double e0;
+//! Electron mass (kg)
+static const double me;
+//! Electron volt (J)
+static const double eV;
+//! Elementary charge (C)
+static const double e;
+//! Faraday constant (C.mol-1)
+static const double F;
+//! Fine-structure constant
+static const double a;
+//! Molar gas constant (J.mol-1.K-1)
+static const double R;
+//! Stefan-Boltzmann constant (W.m-2.K-4)
+static const double s;
+};
+
+const double PhysicalConstants::mu = double(1.66054e-27);
+const double PhysicalConstants::Na = double(6.02214e+23);
+const double PhysicalConstants::kb = double(1.38065e-23);
+const double PhysicalConstants::G0 = double(7.74809e-05);
+const double PhysicalConstants::e0 = double(8.85419e-12);
+const double PhysicalConstants::me = double(9.10938e-31);
+const double PhysicalConstants::eV = double(1.60218e-19);
+const double PhysicalConstants::e = double(1.60218e-19);
+const double PhysicalConstants::F = double(96485.3);
+const double PhysicalConstants::a = double(0.00729735);
+const double PhysicalConstants::R = double(8.31446);
+const double PhysicalConstants::s = double(5.67037e-08);
+
+} // end of namespace tfel
+void declarePhysicalConstants()
+{
+  boost::python::class_<tfel::PhysicalConstants>("PhysicalConstants")
+.def_readonly("mu",tfel::PhysicalConstants::mu,
+"Atomic mass constant (kg)")
+.def_readonly("Na",tfel::PhysicalConstants::Na,
+"Avogadro constant (mol-1)")
+.def_readonly("kb",tfel::PhysicalConstants::kb,
+"Boltzmann constant (J.K-1)")
+.def_readonly("G0",tfel::PhysicalConstants::G0,
+"Conductance quantum (S)")
+.def_readonly("e0",tfel::PhysicalConstants::e0,
+"Electric constant (F.m-1)")
+.def_readonly("me",tfel::PhysicalConstants::me,
+"Electron mass (kg)")
+.def_readonly("eV",tfel::PhysicalConstants::eV,
+"Electron volt (J)")
+.def_readonly("e",tfel::PhysicalConstants::e,
+"Elementary charge (C)")
+.def_readonly("F",tfel::PhysicalConstants::F,
+"Faraday constant (C.mol-1)")
+.def_readonly("a",tfel::PhysicalConstants::a,
+"Fine-structure constant")
+.def_readonly("R",tfel::PhysicalConstants::R,
+"Molar gas constant (J.mol-1.K-1)")
+.def_readonly("s",tfel::PhysicalConstants::s,
+"Stefan-Boltzmann constant (W.m-2.K-4)")
+;
+
+} // end of declarePhysicalConstants
+
---
+++ include/TFEL/PhysicalConstants.hxx
@@ -0,0 +1,43 @@
+/*!
+ * \file   PhysicalConstants.hxx
+ * \brief
+ * \author HELFER Thomas 202608
+ * \date   18 août 2017
+ */
+
+#ifndef LIB_TFEL_PHYSICALCONSTANTS_HXX
+#define LIB_TFEL_PHYSICALCONSTANTS_HXX
+
+namespace tfel{
+
+  template<typename real = double,bool use_qt = false>
+  struct PhysicalConstants{
+    //! Atomic mass constant (kg)
+    static constexpr const real mu = real(1.66054e-27);
+    //! Avogadro constant (mol-1)
+    static constexpr const real Na = real(6.02214e+23);
+    //! Boltzmann constant (J.K-1)
+    static constexpr const real kb = real(1.38065e-23);
+    //! Conductance quantum (S)
+    static constexpr const real G0 = real(7.74809e-05);
+    //! Electric constant (F.m-1)
+    static constexpr const real e0 = real(8.85419e-12);
+    //! Electron mass (kg)
+    static constexpr const real me = real(9.10938e-31);
+    //! Electron volt (J)
+    static constexpr const real eV = real(1.60218e-19);
+    //! Elementary charge (C)
+    static constexpr const real e = real(1.60218e-19);
+    //! Faraday constant (C.mol-1)
+    static constexpr const real F = real(96485.3);
+    //! Fine-structure constant
+    static constexpr const real a = real(0.00729735);
+    //! Molar gas constant (J.mol-1.K-1)
+    static constexpr const real R = real(8.31446);
+    //! Stefan-Boltzmann constant (W.m-2.K-4)
+    static constexpr const real s = real(5.67037e-08);
+  }; // end of PhysicalConstants
+
+} // end of namespace tfel
+
+#endif /* LIB_TFEL_PHYSICALCONSTANTS_HXX */
