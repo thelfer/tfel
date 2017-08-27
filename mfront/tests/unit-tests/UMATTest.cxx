f4664e7140a45d78107c9d56ab679aae7d7b52dd
th202608 <th202608@localhost>
2017-08-27 14:31:41 +0000

    add missing files

---
+++ mfront/tests/unit-tests/UMATTest.cxx
@@ -0,0 +1,73 @@
+/*!
+ * \file   mfront/tests/unit-tests/UMATTest.cxx
+ * \brief    
+ * \author Thomas Helfer
+ * \date   25 août 2017
+ */
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif /* NDEBUG */
+
+#include<cstdlib>
+#include<iostream>
+#include<stdexcept>
+
+#include"TFEL/Tests/TestCase.hxx"
+#include"TFEL/Tests/TestProxy.hxx"
+#include"TFEL/Tests/TestManager.hxx"
+
+#include"umat.cpp"
+
+struct UMATTest final
+  : public tfel::tests::TestCase
+{
+  UMATTest()
+    : tfel::tests::TestCase("MFront","UMATTest")
+  {} // end of UMATTest
+
+  tfel::tests::TestResult
+  execute() override
+  {
+    char n[80] = {'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',
+		  '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',
+		  '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',
+		  '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',
+		  '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',
+		  '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',
+		  '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',
+		  '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0'};
+    const char * b  = "UMATBEHAVIOUR_CHABOCHE_3D_1";
+    const char * b2 = "UMATBEHAVIOUR_CHABOCHE_PSTRAIN";
+    std::copy(b,b+std::strlen(b),n);
+    const auto r = decompose(n);
+#ifdef _WIN32			 
+    TFEL_TESTS_ASSERT(r.first=="libUMATBEHAVIOUR.dll");
+#else
+    TFEL_TESTS_ASSERT(r.first=="libUMATBEHAVIOUR.so");
+#endif /* _WIN32 */
+    TFEL_TESTS_ASSERT(r.second=="CHABOCHE_3D");
+    // second test
+    std::fill(n,n+80,'\0');
+    std::copy(b2,b2+std::strlen(b2),n);
+    const auto r2 = decompose(n);
+#ifdef _WIN32			 
+    TFEL_TESTS_ASSERT(r2.first=="libUMATBEHAVIOUR.dll");
+#else
+    TFEL_TESTS_ASSERT(r2.first=="libUMATBEHAVIOUR.so");
+#endif /* _WIN32 */
+    TFEL_TESTS_ASSERT(r2.second=="CHABOCHE_PSTRAIN");
+    return this->result;
+  } // end of execute()
+};
+
+TFEL_TESTS_GENERATE_PROXY(UMATTest,"UMATTest");
+
+/* coverity [UNCAUGHT_EXCEPT]*/
+int main()
+{
+  auto& m = tfel::tests::TestManager::getTestManager();
+  m.addTestOutput(std::cout);
+  m.addXMLTestOutput("UMATTest.xml");
+  return m.execute().success() ? EXIT_SUCCESS : EXIT_FAILURE;
+} // end of main
