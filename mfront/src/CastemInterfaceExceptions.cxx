7fcf70dca938798ff0db240982dbe8674f084232
th202608 <th202608@localhost>
2018-09-07 16:35:52 +0000

    Fix checkNTENSValue (performance issue)

--- mfront/src/CastemInterfaceExceptions.cxx
+++ mfront/src/CastemInterfaceExceptions.cxx
@@ -149,14 +149,16 @@ namespace castem {
 
   void CastemInterfaceExceptions::checkNTENSValue(const CastemInt NTENS,
                                                   const unsigned short s) {
-    tfel::raise_if(NTENS != s,
-                   "CastemInterfaceExceptions::checkNTENSValue : "
-                   "invalid value for the NTENS parameter "
-                   "(got '" +
-                       std::to_string(NTENS) +
-                       "', "
-                       "expected '" +
-                       std::to_string(static_cast<unsigned int>(s)) + "')");
+    if (NTENS != s) {
+      tfel::raise(
+          "CastemInterfaceExceptions::checkNTENSValue : "
+          "invalid value for the NTENS parameter "
+          "(got '" +
+          std::to_string(NTENS) +
+          "', "
+          "expected '" +
+          std::to_string(static_cast<unsigned int>(s)) + "')");
+    }
   }  // end of CastemInterfaceExceptions::checkNTENSValue
 
   void
