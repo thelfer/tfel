c04079fa9940fc4f85b3d649cd75b12503913ec5
th202608 <th202608@localhost>
2020-03-24 11:23:06 +0000

    code clean-up

--- mfront/include/MFront/GenericBehaviour/BehaviourData.hxx
+++ mfront/include/MFront/GenericBehaviour/BehaviourData.hxx
@@ -18,10 +18,10 @@
 #include "MFront/GenericBehaviour/Types.hxx"
 #include "MFront/GenericBehaviour/BehaviourData.h"
 
-namespace mfront{
-  
-  namespace gb{
-    
+namespace mfront {
+
+  namespace gb {
+
     //! a simple alias
     using BehaviourData = ::mfront_gb_BehaviourData;
 
@@ -32,7 +32,7 @@ namespace mfront{
      * \tparam b: true for isotropic elastic behaviours, false for
      * orthotropic elasticbehaviours
      */
-    template<tfel::material::ModellingHypothesis::Hypothesis, const bool>
+    template <tfel::material::ModellingHypothesis::Hypothesis, const bool>
     struct ElasticMaterialPropertiesNumber;
 
     /*!
@@ -41,83 +41,91 @@ namespace mfront{
      * \tparam b: true for isotropic elastic behaviours, false for
      * orthotropic elasticbehaviours
      */
-    template<const bool>
+    template <const bool>
     struct ThermalExpansionCoefficientPropertiesNumber;
 
-    template<>
-    struct ThermalExpansionCoefficientPropertiesNumber<true>{
+    template <>
+    struct ThermalExpansionCoefficientPropertiesNumber<true> {
       static constexpr const unsigned short value = 1u;
-    }; // end of struct ThermalExpansionCoefficientPropertiesNumber<true>
+    };  // end of struct ThermalExpansionCoefficientPropertiesNumber<true>
 
-    template<>
-    struct ThermalExpansionCoefficientPropertiesNumber<false>{
+    template <>
+    struct ThermalExpansionCoefficientPropertiesNumber<false> {
       static constexpr const unsigned short value = 3u;
-    }; // end of struct ThermalExpansionCoefficientPropertiesNumber<false>
-    
-    template<unsigned short N>
+    };  // end of struct ThermalExpansionCoefficientPropertiesNumber<false>
+
+    template <unsigned short N>
     struct OrthotropicElasticMaterialPropertiesNumber;
 
-    template<>
-    struct OrthotropicElasticMaterialPropertiesNumber<1u>{
+    template <>
+    struct OrthotropicElasticMaterialPropertiesNumber<1u> {
       static constexpr const unsigned short value = 6u;
-    }; // end of OrthotropicElasticMaterialPropertiesNumber<1u>
+    };  // end of OrthotropicElasticMaterialPropertiesNumber<1u>
 
-    template<>
-    struct OrthotropicElasticMaterialPropertiesNumber<2u>{
+    template <>
+    struct OrthotropicElasticMaterialPropertiesNumber<2u> {
       static constexpr const unsigned short value = 7u;
-    }; // end of struct OrthotropicElasticMaterialPropertiesNumber<2u>
+    };  // end of struct OrthotropicElasticMaterialPropertiesNumber<2u>
 
-    template<>
-    struct OrthotropicElasticMaterialPropertiesNumber<3u>{
+    template <>
+    struct OrthotropicElasticMaterialPropertiesNumber<3u> {
       static constexpr const unsigned short value = 9u;
-    }; // end of OrthotropicElasticMaterialPropertiesNumber<3u>
-    
+    };  // end of OrthotropicElasticMaterialPropertiesNumber<3u>
+
     //! \brief partial specialisation for elastic isotropic behaviour
-    template<tfel::material::ModellingHypothesis::Hypothesis H>
-    struct ElasticMaterialPropertiesNumber<H,true>{
+    template <tfel::material::ModellingHypothesis::Hypothesis H>
+    struct ElasticMaterialPropertiesNumber<H, true> {
       static constexpr const unsigned short value = 2u;
-    }; // end of ElasticMaterialPropertiesNumber<H,true>
+    };  // end of ElasticMaterialPropertiesNumber<H,true>
 
     //! \brief partial specialisation for elastic isotropic behaviour
-    template<tfel::material::ModellingHypothesis::Hypothesis H>
-    struct ElasticMaterialPropertiesNumber<H,false>
-      : OrthotropicElasticMaterialPropertiesNumber<tfel::material::ModellingHypothesisToSpaceDimension<H>::value>
-    {}; // end of ElasticMaterialPropertiesNumber<H,false>
+    template <tfel::material::ModellingHypothesis::Hypothesis H>
+    struct ElasticMaterialPropertiesNumber<H, false>
+        : OrthotropicElasticMaterialPropertiesNumber<
+              tfel::material::ModellingHypothesisToSpaceDimension<H>::value> {
+    };  // end of ElasticMaterialPropertiesNumber<H,false>
 
-    template<tfel::material::ModellingHypothesis::Hypothesis H,
-	     const bool,const bool,const bool,const bool>
+    template <tfel::material::ModellingHypothesis::Hypothesis H,
+              const bool,
+              const bool,
+              const bool,
+              const bool>
     struct ThermoElasticMaterialPropertiesNumber;
 
-    template<tfel::material::ModellingHypothesis::Hypothesis H,
-	     const bool es,const bool bs>
-    struct ThermoElasticMaterialPropertiesNumber<H,false,es,false,bs>{
+    template <tfel::material::ModellingHypothesis::Hypothesis H,
+              const bool es,
+              const bool bs>
+    struct ThermoElasticMaterialPropertiesNumber<H, false, es, false, bs> {
       static constexpr const unsigned short value = 0u;
     };
 
-    template<tfel::material::ModellingHypothesis::Hypothesis H,
-	     const bool es,const bool bs>
-    struct ThermoElasticMaterialPropertiesNumber<H,true,es,false,bs>{
+    template <tfel::material::ModellingHypothesis::Hypothesis H,
+              const bool es,
+              const bool bs>
+    struct ThermoElasticMaterialPropertiesNumber<H, true, es, false, bs> {
       static constexpr const unsigned short value =
-	ElasticMaterialPropertiesNumber<H,es>::value;
+          ElasticMaterialPropertiesNumber<H, es>::value;
     };
 
-    template<tfel::material::ModellingHypothesis::Hypothesis H,
-	     const bool es,const bool bs>
-    struct ThermoElasticMaterialPropertiesNumber<H,false,es,true,bs>{
+    template <tfel::material::ModellingHypothesis::Hypothesis H,
+              const bool es,
+              const bool bs>
+    struct ThermoElasticMaterialPropertiesNumber<H, false, es, true, bs> {
       static constexpr const unsigned short value =
-	ThermalExpansionCoefficientPropertiesNumber<bs>::value;
+          ThermalExpansionCoefficientPropertiesNumber<bs>::value;
     };
 
-    template<tfel::material::ModellingHypothesis::Hypothesis H,
-	     const bool es,const bool bs>
-    struct ThermoElasticMaterialPropertiesNumber<H,true,es,true,bs>{
+    template <tfel::material::ModellingHypothesis::Hypothesis H,
+              const bool es,
+              const bool bs>
+    struct ThermoElasticMaterialPropertiesNumber<H, true, es, true, bs> {
       static constexpr const unsigned short value =
-	ElasticMaterialPropertiesNumber<H,es>::value+
-	ThermalExpansionCoefficientPropertiesNumber<bs>::value;
+          ElasticMaterialPropertiesNumber<H, es>::value +
+          ThermalExpansionCoefficientPropertiesNumber<bs>::value;
     };
-    
-  } // end of namespace gb
-  
-} // end of namespace mfront
+
+  }  // end of namespace gb
+
+}  // end of namespace mfront
 
 #endif /* LIB_MFRONT_GENERICBEHAVIOUR_BEHAVIOURDATA_HXX */
