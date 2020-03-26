9e237b6d49d94abe709e7bf93993311ac43cf4df
th202608 <th202608@localhost>
2020-03-26 21:02:43 +0000

    Fix mfront/src/PlasticInelasticFlow.cxx

--- mfront/src/PlasticInelasticFlow.cxx
+++ mfront/src/PlasticInelasticFlow.cxx
@@ -68,13 +68,16 @@ namespace mfront {
         c += sp.computeDerivatives(bd, "strain", "p" + id,
                                    dseq_ds + "/(" + snf + ")",
                                    this->sc->isNormalDeviatoric());
-        c += "if((" + dR_ddp + ">0)&&(seq<" +
-             sp.getEquivalentStressLowerBound(bd) + ")){\n";
+        c += "if(seq<" + sp.getEquivalentStressLowerBound(bd) + "){\n";
+        c += "if(" + dR_ddp + ">0){\n";
         c += dfp_ddp + " = -1*std::max(real(1.e-12),(" + dR_ddp + ")/(" + snf +
              "));\n";
         c += "} else {\n";
         c += dfp_ddp + " = -1*std::min(-real(1.e-12),(" + dR_ddp + ")/(" + snf +
              "));\n";
+        c += "}\n";
+        c += "} else {\n";
+        c += dfp_ddp + " = -(" + dR_ddp + ")/(" + snf + ");\n";
         c += "}\n";
         auto kid = decltype(khrs.size()){};
         for (const auto& khr : khrs) {
