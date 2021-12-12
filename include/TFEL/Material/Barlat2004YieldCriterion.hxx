bf774650be7ae342d661899492481efad784e384
Thomas Helfer <thomas.helfer@cea.fr>
2021-12-13 00:29:59 +0100

    Fix makeBarlatLinearTransformation to use fsarray rather than tvector du to Issue #54

--- include/TFEL/Material/Barlat2004YieldCriterion.hxx
+++ include/TFEL/Material/Barlat2004YieldCriterion.hxx
@@ -194,7 +194,7 @@ namespace tfel::material {
    */
   template <unsigned short N, typename real>
   tfel::math::st2tost2<N, real> makeBarlatLinearTransformation(
-      const tfel::math::tvector<9u, real>&);
+      const tfel::math::fsarray<9u, real>&);
   /*!
    * \tparam H: modelling hypothesis
    * \tparam c: orthotropic axis convention
@@ -296,7 +296,7 @@ namespace tfel::material {
             OrthotropicAxesConvention,
             typename real>
   tfel::math::st2tost2<ModellingHypothesisToSpaceDimension<H>::value, real>
-  makeBarlatLinearTransformation(const tfel::math::tvector<9u, real>&);
+  makeBarlatLinearTransformation(const tfel::math::fsarray<9u, real>&);
   /*!
    * \brief This function computes the Barlat yield stress.
    * The Barlat yield stress is defined by:
