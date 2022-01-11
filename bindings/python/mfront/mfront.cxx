74b08b00fb171ae0a396138e632db5af9ba73c26
Thomas Helfer <thomas.helfer@cea.fr>
2022-01-11 21:08:36 +0100

    Fix symbol declaration

--- bindings/python/mfront/mfront.cxx
+++ bindings/python/mfront/mfront.cxx
@@ -63,7 +63,7 @@ void declareAbstractBehaviourDSL();
 void declareBehaviourSymmetryType();
 void declareVariableBoundsDescription();
 void declareVariableDescription();
-void declareBehaviourAttribute();
+void declareMaterialKnowledgeDescriptionAttribute();
 void declareBehaviourData();
 void declareMaterialPropertyDescription();
 void declareBehaviourDescription();
@@ -113,7 +113,7 @@ BOOST_PYTHON_MODULE(_mfront) {
   declareMaterialPropertyDescription();
   // behaviours
   declareAbstractBehaviourDSL();
-  declareBehaviourAttribute();
+  declareMaterialKnowledgeDescriptionAttribute();
   declareVariableBoundsDescription();
   declareVariableDescription();
   declareBehaviourData();
