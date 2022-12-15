1a9405253b8abddd7310b2f020e253417c0f492e
Thomas Helfer <thomas.helfer@cea.fr>
2022-12-15 13:00:41 +0100

    Fix Issue #352

--- mfront/src/UMATInterfaceBase.cxx
+++ mfront/src/UMATInterfaceBase.cxx
@@ -1105,9 +1105,9 @@ namespace mfront {
     for (const auto& m : mprops.first) {
       auto flag = SupportedTypes::getTypeFlag(m.type);
       tfel::raise_if(flag != SupportedTypes::SCALAR,
-                     "UMATInterfaceBase::generateFile2: "
-                     "unsupported external state variable type "
-                     "in mtest file generation");
+          "UMATInterfaceBase::generateMTestFileForHypothesis: "
+          "unsupported external state variable type "
+          "in mtest file generation");
       if (m.arraySize == 1u) {
         if (offset == 0) {
           out << "mg.addMaterialProperty(\"" << m.name << "\",*(PROPS));\n";
@@ -1132,6 +1132,10 @@ namespace mfront {
     for (const auto& v : persistentVarsHolder) {
       auto flag = SupportedTypes::getTypeFlag(v.type);
       const auto& ivname = d.getExternalName(v.name);
+      tfel::raise_if((flag != SupportedTypes::STENSOR) && (flag != SupportedTypes::SCALAR),
+                     "UMATInterfaceBase::generateFile2: "
+                     "unsupported material property type "
+                     "in mtest file generation");
       if (v.arraySize == 1u) {
         if (flag == SupportedTypes::SCALAR) {
           out << "mg.addInternalStateVariable(\"" << ivname
