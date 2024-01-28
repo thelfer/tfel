6ece302c359969d1f342c5da0e8b2dee222f9a4c
Thomas Helfer <thomas.helfer@cea.fr>
2024-01-28 22:37:13 +0100

    Fix Issue #487

--- tfel-config/src/tfel-config.cxx
+++ tfel-config/src/tfel-config.cxx
@@ -257,7 +257,7 @@ static void listOptions(std::ostream& os) {
 
 TFEL_NORETURN static void treatVersion() {
   std::cout << "tfel-config " << VERSION
-            << " (svn revision : " << TFEL_SVN_REVISION << ")" << std::endl;
+            << " (git hash: " << TFEL_GIT_HASH << ")" << std::endl;
   std::exit(EXIT_SUCCESS);
 }  // end of treatHelp
 
