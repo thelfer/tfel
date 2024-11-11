8980afa8c2fb62be2f923756406409ead8060e70
Thomas Helfer <thomas.helfer@cea.fr>
2024-11-11 21:35:58 +0100

    remove MD flags

--- cmake/modules/clang.cmake
+++ cmake/modules/clang.cmake
@@ -36,7 +36,7 @@ endif(enable-fast-math)
 if(NOT WIN32)
 tfel_add_cxx_compiler_flag_if_available(VISIBILITY_FLAGS "fvisibility=hidden")
 tfel_add_cxx_compiler_flag_if_available(VISIBILITY_FLAGS "fvisibility-inlines-hidden")
-tfel_add_cxx_compiler_flag_if_available(COMPILER_CXXFLAGS "MD")
+# tfel_add_cxx_compiler_flag_if_available(COMPILER_CXXFLAGS "MD")
 endif(NOT WIN32)
 
 set(OPTIMISATION_FLAGS "-DTFEL_NO_RUNTIME_CHECK_BOUNDS ${OPTIMISATION_FLAGS}")
