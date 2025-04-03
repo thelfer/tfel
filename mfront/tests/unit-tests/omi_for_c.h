3fcf77bd43562b74eb561d66da6bfe5959368953
Thomas Helfer <thomas.helfer@cea.fr>
2025-04-03 12:26:52 +0200

    add missing file

---
+++ mfront/tests/unit-tests/omi_for_c.h
@@ -0,0 +1,13 @@
+/*!
+ * \file   omi_for_c.h
+ * \brief    
+ * \author HELFER Thomas
+ * \date   03 avril 2025
+ */
+
+#ifndef LIB_OMI_FOR_C_H
+#define LIB_OMI_FOR_C_H
+
+#define FOR_NAME(fct, FCT) fct_
+
+#endif /* LIB_OMI_FOR_C_H */
