62d5c1a67c85d0871e745d21c020bb98b256cf92
Thomas Helfer <thomas.helfer@cea.fr>
2025-06-10 10:16:43 +0200

    Fix Issue #743

--- mtest/src/SingleStructureSchemeParser.cxx
+++ mtest/src/SingleStructureSchemeParser.cxx
@@ -204,7 +204,6 @@ namespace mtest {
     }
     this->readSpecifiedToken("SingleStructureSchemeParser::handleBehaviour",
                              ";", p, this->tokens.end());
-    mfront::getLogStream() << l << " " << f << std::endl;
     if (w.empty()) {
       t.setBehaviour(i, l, f, d);
     } else {
