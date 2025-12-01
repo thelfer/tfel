d0447e258adcbce3e076160db866bdeb1f60d521
Thomas Helfer <thomas.helfer@cea.fr>
2025-12-01 22:43:44 +0100

    Fix Issue #884

--- mfront/src/IsotropicMisesPlasticFlowDSL.cxx
+++ mfront/src/IsotropicMisesPlasticFlowDSL.cxx
@@ -106,12 +106,13 @@ namespace mfront {
 
   void IsotropicMisesPlasticFlowDSL::endsInputFileProcessing() {
     IsotropicBehaviourDSLBase::endsInputFileProcessing();
-    const auto h = ModellingHypothesis::UNDEFINEDHYPOTHESIS;
-    if (!this->mb.hasCode(h, BehaviourData::FlowRule)) {
-      this->throwRuntimeError(
-          "IsotropicMisesCreepDSL::"
-          "endsInputFileProcessing",
-          "no flow rule defined");
+    for (const auto& h : this->mb.getDistinctModellingHypotheses()) {
+      if (!this->mb.hasCode(h, BehaviourData::FlowRule)) {
+        this->throwRuntimeError(
+            "IsotropicMisesCreepDSL::"
+            "endsInputFileProcessing",
+            "no flow rule defined");
+      }
     }
   }  // end of endsInputFileProcessing
 
