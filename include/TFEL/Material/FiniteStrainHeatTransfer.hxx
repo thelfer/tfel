f07a26ca3089b5a0f86253ccf1901f70b5599a12
Thomas Helfer <thomas.helfer@cea.fr>
2026-02-04 22:29:53 +0100

    first implementation and test

--- include/TFEL/Material/FiniteStrainHeatTransfer.hxx
+++ include/TFEL/Material/FiniteStrainHeatTransfer.hxx
@@ -17,17 +17,79 @@ namespace tfel::material {
    * \brief compute the thermal conductivity matrix in the reference
    * configuration described by the isotropic Fourier law in the current
    * configuration.
+   *
    * \param[in] k: thermal conductivity
    * \param[in] F: deformation gradient
+   *
+   * \note This function can also compute the derivative of the the thermal
+   * conductivity matrix with respect to the temperature.
+   * \note This function is also suitable for orthotropic materials in 1D
    */
-  template <tfel::math::ScalarConcept ThermalConductivityType,
+  template <tfel::math::ScalarConcept ThermalConductivityOrDerivativeType,
             tfel::math::NoUnitTensorConcept DeformationGradientType>
-  requires(
-      tfel::math::checkUnitCompatibility<tfel::math::unit::ThermalConductivity,
-                                         ThermalConductivityType>())  //
-      TFEL_HOST_DEVICE
-      constexpr auto computeThermalConductivyMatrixInReferenceConfiguration(
-          const ThermalConductivityType,
+  requires((tfel::math::checkUnitCompatibility<
+               tfel::math::unit::ThermalConductivity,
+               ThermalConductivityOrDerivativeType>()) ||
+           (tfel::math::checkUnitCompatibility<
+               tfel::math::unit::generate_unit<1, 1, -3, 0, -2, 0, 0>,
+               ThermalConductivityOrDerivativeType>()))  //
+      TFEL_HOST_DEVICE [[nodiscard]] constexpr auto      //
+      computeThermalConductivyMatrixInReferenceConfiguration(
+          const ThermalConductivityOrDerivativeType,
+          const DeformationGradientType&) noexcept;
+  /*!
+   * \brief compute the thermal conductivity matrix in the reference
+   * configuration described by the orthotropic Fourier law in the current
+   * configuration.
+   *
+   * \param[in] k1: thermal conductivity in the first direction
+   * \param[in] k2: thermal conductivity in the second direction
+   * \param[in] F: deformation gradient
+   *
+   * \note This function can also compute the derivative of the the thermal
+   * conductivity matrix with respect to the temperature.
+   */
+  template <tfel::math::ScalarConcept ThermalConductivityOrDerivativeType,
+            tfel::math::NoUnitTensorConcept DeformationGradientType>
+  requires(((tfel::math::checkUnitCompatibility<
+                tfel::math::unit::ThermalConductivity,
+                ThermalConductivityOrDerivativeType>()) ||
+            (tfel::math::checkUnitCompatibility<
+                tfel::math::unit::generate_unit<1, 1, -3, 0, -2, 0, 0>,
+                ThermalConductivityOrDerivativeType>())) &&
+           (tfel::math::space_dimension<DeformationGradientType> == 2u))  //
+      TFEL_HOST_DEVICE [[nodiscard]] constexpr auto                       //
+      computeThermalConductivyMatrixInReferenceConfiguration(
+          const ThermalConductivityOrDerivativeType,
+          const ThermalConductivityOrDerivativeType,
+          const DeformationGradientType&) noexcept;
+  /*!
+   * \brief compute the thermal conductivity matrix in the reference
+   * configuration described by the orthotropic Fourier law in the current
+   * configuration.
+   *
+   * \param[in] k1: thermal conductivity in the first direction
+   * \param[in] k2: thermal conductivity in the second direction
+   * \param[in] k3: thermal conductivity in the third direction
+   * \param[in] F: deformation gradient
+   *
+   * \note This function can also compute the derivative of the the thermal
+   * conductivity matrix with respect to the temperature.
+   */
+  template <tfel::math::ScalarConcept ThermalConductivityOrDerivativeType,
+            tfel::math::NoUnitTensorConcept DeformationGradientType>
+  requires(((tfel::math::checkUnitCompatibility<
+                tfel::math::unit::ThermalConductivity,
+                ThermalConductivityOrDerivativeType>()) ||
+            (tfel::math::checkUnitCompatibility<
+                tfel::math::unit::generate_unit<1, 1, -3, 0, -2, 0, 0>,
+                ThermalConductivityOrDerivativeType>())) &&
+           (tfel::math::space_dimension<DeformationGradientType> == 3u))  //
+      TFEL_HOST_DEVICE [[nodiscard]] constexpr auto                       //
+      computeThermalConductivyMatrixInReferenceConfiguration(
+          const ThermalConductivityOrDerivativeType,
+          const ThermalConductivityOrDerivativeType,
+          const ThermalConductivityOrDerivativeType,
           const DeformationGradientType&) noexcept;
 
 }  // end of namespace tfel::material
